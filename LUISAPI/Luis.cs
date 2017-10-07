26c39df5d024736fcd61b5dc77a5408f7bf88ae5
Cooper W Gilliland <db.cooper_94@live.com>
2017-10-06 18:10:19 -0700

    Updated the LUIS library with some test code

---
+++ LUISAPI/Luis.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Cognitive.LUIS;
+
+namespace LUISAPI
+{
+    public class Luis
+    {
+        public event EventHandler<LuisUtteranceResultEventArgs> OnLuisUtteranceResultUpdated; 
+        private LuisClient _luisClient;
+
+        public Luis(LuisClient luisClient)
+        {
+            _luisClient = luisClient;
+        }
+
+        private void RaiseOnLuisUtteranceResultUpdated(LuisUtteranceResultEventArgs args)
+        {
+            OnLuisUtteranceResultUpdated?.Invoke(this, args);
+        }
+        public async Task RequestAsync(string input)
+        {
+            try
+            {
+                LuisResult result = await _luisClient.Predict(input);
+                ProcessResult(result);
+            }
+            catch (Exception e)
+            {
+                RaiseOnLuisUtteranceResultUpdated(new LuisUtteranceResultEventArgs{Status = "Failed", Message = e.Message});
+            }
+        }
+
+        private void ProcessResult(LuisResult result)
+        {
+            LuisUtteranceResultEventArgs args = new LuisUtteranceResultEventArgs();
+            args.Result = result; //may need to cut
+            args.Status = "Succeeded";
+            args.Message =
+                $"Top intent is {result.TopScoringIntent.Name} " +
+                $"with score {result.TopScoringIntent.Score}. Found " +
+                $"{result.Entities.Count} entities.";
+            RaiseOnLuisUtteranceResultUpdated(args);
+        }
+    }
+}
---
+++ LUISAPI/LuisUtteranceResultEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Cognitive.LUIS;
+
+namespace LUISAPI
+{
+    public class LuisUtteranceResultEventArgs : EventArgs
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public LuisResult Result { get; set; } //May need to cut
+        public bool RequiresReply { get; set; }
+    }
+}
