de7a3b5bd7bf46a61d2f30f5d6841e373c9af751
Cooper W Gilliland <db.cooper_94@live.com>
2017-10-21 16:55:41 -0700

    Setiment, language determination, and key phrases added

---
+++ Gilliland_Sentiment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAnalysisAPI;
+
+namespace Gilliland_Sentiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("This Application takes raw text input and determines\n" +
+                                  "the language, key phrases (if language is supported)\n"
+                                  +"and sentiment. Sentiment is scored between 0 and 1\n"
+                                  + "with 0 being negative and 1 being positive\n");
+                Analyze textAnalyze = new Analyze();
+                string input = "";
+                bool cont = false;   
+                //loop to collect multiple lines of text such as when text is copy pasted 
+                while (!cont)
+                {
+                    Console.Write("-> ");                   
+                    string holder = Console.ReadLine();    //collect user input              
+                    if (holder == "y")
+                    {
+                        cont = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Finished typing? Enter [y]es to continue to the analysis, or enter your next line of text.");
+                        //aggragate input 
+                        input += holder;
+                    }
+
+                }    
+                //hand the user input to the analyzer 
+                textAnalyze.Gambit(input);
+                Console.WriteLine("\nPress Enter to Quit");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- TextAnalysisAPI/Analyze.cs
+++ TextAnalysisAPI/Analyze.cs
@@ -3,10 +3,110 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
 
 namespace TextAnalysisAPI
 {
     public class Analyze
     {
+        private const string apiKey = "ab37198f91c64e839bbd8bb21edf9bd6";
+        private ITextAnalyticsAPI apiClient;
+        private string language = "";
+        /// <summary>
+        /// initilize the API object being used by the class
+        /// </summary>
+        public Analyze()
+        {         
+            apiClient = new TextAnalyticsAPI
+            {
+                //give the api the needed region and keys 
+                AzureRegion = AzureRegions.Westus,
+                SubscriptionKey = apiKey
+            };            
+        }
+        /// <summary>
+        /// wrap access to other private functions 
+        /// </summary>
+        /// <param name="dataToAnalyze"></param>
+        public void Gambit(string dataToAnalyze)
+        {
+            DetermineLanguage(dataToAnalyze);
+            DetermineKeyPhrases(dataToAnalyze);
+            DetermineSentiment(dataToAnalyze);
+        }
+        /// <summary>
+        /// determine the language of the incoming text
+        /// this determination will be used by later functions
+        /// </summary>
+        /// <param name="incoming"></param>
+        private void DetermineLanguage(string incoming)
+        {
+            //call the language detector from the azure library 
+            //hand it a batch of lists containing the data 
+            //NOTE: this is currently the only method of doing this with the 
+            //Azure libraries
+            LanguageBatchResult langRes = apiClient.DetectLanguage(
+                new BatchInput(
+                    new List<Input>()
+                    {
+                        new Input("1", incoming) 
+                    }));
+            //iterate through results 
+            foreach (var document in langRes.Documents)
+            {
+                //Output the detected language
+                Console.WriteLine("Language: {0}", document.DetectedLanguages[0].Name);
+                language = document.DetectedLanguages[0].Iso6391Name; //assign the language to a class variable for later use
+            }
+        }
+        /// <summary>
+        /// determine the key phrases in incoming text
+        /// </summary>
+        /// <param name="incoming"></param>
+        private void DetermineKeyPhrases(string incoming)
+        {
+            //call the key phrase detector from the azure library 
+            KeyPhraseBatchResult PhRes = apiClient.KeyPhrases(
+                new MultiLanguageBatchInput(
+                    new List<MultiLanguageInput>()
+                    {
+                        //use the language code determined by DetermineLanguage()
+                        new MultiLanguageInput(language, "1", incoming)
+                    }));
+            //iterate through results 
+            foreach (var document in PhRes.Documents)
+            {
+                Console.WriteLine("Key phrases:");
+                //iterate through key phrases
+                foreach (string keyphrase in document.KeyPhrases)
+                {
+                    Console.WriteLine("\t" + keyphrase);
+                }
+            }
+        }
+        /// <summary>
+        /// use the Azure libraries to determine the sentiment of user input 
+        /// </summary>
+        /// <param name="incoming"></param>
+        private void DetermineSentiment(string incoming)
+        {
+            //create a class intem to hold the incoming results
+            //and call the Sentiment function of the api library 
+            //this function takes the arguement of a Multi language batch input
+            SentimentBatchResult result = apiClient.Sentiment(
+                new MultiLanguageBatchInput(
+                    new List<MultiLanguageInput>()
+                    {
+                        //use the language code determined by DetermineLanguage()
+                        new MultiLanguageInput(language,"0", incoming)
+                    }));
+            //iterate through results 
+            foreach (var document in result.Documents)
+            {
+                //output the sentiment determined by the api
+                Console.WriteLine("Sentiment Score: {0:0.00}", document.Score);
+            }
+        }
     }
 }
